d0bae9793b13759226b5282889685d82012211f9
Kroshka23 <ahmedpubg2005@yandex.ru>
2022-12-06 21:42:37 +0300

    Kroshka23.github.io

---
+++ up.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+const testimonials = document.querySelector('.testimonials')
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('disp');
+
+   });
+
